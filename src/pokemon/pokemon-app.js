1a42b9e08439f34d4bd573c3965849ca73fc03ad
Tobias Vega <tobivega1310@gmail.com>
2026-09-02 17:33:54 -0300

    feat: implementar renderizado de un pokemon por id

--- src/pokemon/pokemon-app.js
+++ src/pokemon/pokemon-app.js
@@ -4,13 +4,39 @@ import { getPokemonById } from "./actions/get-pokemon-by-id.action";
  * Esta función crea la aplicación en el div enviado
  * @param {HTMLDivElement} element 
  */
-export const PokemonApp = (element) => {
+export const PokemonApp = async (element) => {
 
   document.title = 'Pokemon App';
   const titleElement = document.querySelector('#app-title');
 
   titleElement && (titleElement.innerHTML = 'Pokemon App');
 
-  getPokemonById(1);
+  // Referencias HTML
+  const loadingParagraph = document.createElement('p');
+  const pokemonImage = document.createElement('img');
+  const nextBtn = document.createElement('button');
+  const prevBtn = document.createElement('button');
+
+  // Configuraciones
+  loadingParagraph.textContent = 'Cargando...';
+  nextBtn.textContent = 'Siguiente';
+  prevBtn.textContent = 'Anterior';
+
+  element.append(loadingParagraph);
+  element.append(pokemonImage);
+  element.append(nextBtn);
+  element.append(prevBtn);
+
+  //! Listeners de los botones
+
+  // Renderizar el pokemon
+  const renderPokemon = (pokemon) => {
+    pokemonImage.src = pokemon.image;
+    loadingParagraph.textContent = `Pokemon #${pokemon.id} ${pokemon.name}`;
+  }
+
+  //! Hacer la petición inicial
+
+  renderPokemon(await getPokemonById(1));
 
 }
